0bf96769542f6b53175e733cd8e94b784f06a1fb
juan pedro da silva <juanpedrodasilvabarloco@gmail.com>
2023-03-20 15:29:39 -0300

    Added prediction for dataset 7

---
+++ TAYSIR/mlflow_exporter/submit_tools_fix.py
@@ -0,0 +1,51 @@
+import zipfile
+import time
+import tempfile
+import logging
+from pathlib import Path
+from random import choices
+
+import mlflow
+
+logging.getLogger("mlflow").setLevel(logging.DEBUG)
+
+
+class func_to_class(mlflow.pyfunc.PythonModel):
+    def __init__(self, func):
+        self.func = func
+
+    def predict(self, context, model_input):
+        return self.func.predict(model_input)
+
+
+def save_function(func, alphabet_size, prefix):
+    print('Testing function...')
+    alphabet = list(range(0, alphabet_size))
+    sample_input = choices(alphabet, k=25)
+    output = func.predict(sample_input)
+    try:
+        float(output)
+    except ValueError:
+        raise ValueError(
+            f'The output should be an integer or a float, but we found a {type(output).__name__}')
+    print('Test  passed.')
+
+    print('Creating submission...')
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        mlflow_path = Path(tmp_dir)
+        zip_path = f"predicted_models/{prefix}.zip"
+
+        code_paths = list(Path().rglob('*.py'))
+
+        mlflow.pyfunc.save_model(
+            path=mlflow_path,
+            python_model=func_to_class(func),
+            code_path=code_paths
+        )
+
+        with zipfile.ZipFile(zip_path, "w") as zip_file:
+            for f in mlflow_path.rglob('*'):
+                if f.is_file():
+                    zip_file.write(f, f.relative_to(mlflow_path))
+    print(f'Submission created at {zip_path}.')
+    print('You can now submit your model on the competition website.')
---
+++ TAYSIR/utils.py
@@ -0,0 +1,118 @@
+import torch
+
+def predict(sequence, model):
+    """ Define the function that takes a sequence as a list of integers and return the decision of your extracted model. 
+    The function does not need your model as argument. See baseline notebooks for examples."""
+    
+    #For instance, if you want to submit the original model:
+    if hasattr(model, 'predict'): #RNN
+        value = model.predict(model.one_hot_encode(sequence)) 
+        return value
+    else: #Transformer
+        """
+        Note: In this function, add 2 to each int in the word before being input to the model,
+        since ids 0 and 1 are used as special tokens.
+            0 : padding id
+            1 : classification token id
+        Args:
+            word: list of integers 
+        """
+        word = [ [1] + [ a+2 for a in sequence ] ]
+        word = torch.IntTensor(word)
+        with torch.no_grad():
+            out = model(word)
+            return (out.logits.argmax().item())
+
+# We define a class in order to get alphabet, output alphabet and proccess query of PyTorch RNN. 
+# After this we send the model to the generic LStar teacher.
+
+from pythautomata.base_types.sequence import Sequence
+from pythautomata.abstract.model import Model
+from pymodelextractor.teachers.teacher import Teacher
+from pythautomata.base_types.alphabet import Alphabet
+
+class PytorchInference(Model):
+    
+    def __init__(self, alphabet, model, name = "Pytorch NN"):
+        self._alphabet = alphabet
+        self._model = model
+        self._name = name
+        
+    @property
+    def name(self) -> Alphabet:
+        return self._name
+    
+    @property
+    def alphabet(self) -> Alphabet:
+        return self._alphabet
+    
+    @property
+    def output_alphabet(self) -> Alphabet:
+        return Alphabet.from_strings(['1', '0'])
+    
+    def process_query(self, sequence):
+        adapted_sequence = self._adapt_sequence(sequence)
+        return predict(adapted_sequence, self._model) == 1
+    
+    def _adapt_sequence(self, sequence):
+        adapted_seq = []
+        for symbol in sequence.value:
+            adapted_seq.append(int(symbol.value))
+        
+        return adapted_seq
+    
+from pythautomata.utilities.uniform_length_sequence_generator import UniformLengthSequenceGenerator
+
+def test_model(target_model, model, sequence_length = 100, sequence_amount = 10000):
+    sequence_generator = UniformLengthSequenceGenerator(model.alphabet, sequence_length)
+    sequences = sequence_generator.generate_words(sequence_amount)
+    
+    results = []
+    for sequence in sequences:
+        results.append(target_model.process_query(sequence) == model.process_query(sequence))
+    
+    return results
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
