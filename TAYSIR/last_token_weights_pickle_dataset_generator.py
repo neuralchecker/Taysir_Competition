2feee5a92de203da5ab22e0f5fcea14ce386e56c
DESKTOP-36QKRHL\franz <franzmayrojeda@gmail.com>
2023-04-03 10:26:30 -0300

    added dataset generator

---
+++ TAYSIR/last_token_weights_pickle_dataset_generator.py
@@ -0,0 +1,24 @@
+from pythautomata.utilities.uniform_length_sequence_generator import UniformLengthSequenceGenerator
+
+from collections import OrderedDict
+import joblib
+
+
+class LastTokenWeightsPickleDataSetGenerator():
+    def genearte_dataset(self, model, max_query_elements, path ,batch_size = 10_000):
+        total_elements = 0
+        generator = UniformLengthSequenceGenerator(model.alphabet, max_seq_length = 10000000).generate_all_words()
+        symbols = list(model.alphabet.symbols)
+        symbols.sort()
+        symbols = [model.terminal_symbol] + symbols
+        cache = dict()
+        while total_elements<max_query_elements:
+            queries = []
+            for _ in range(batch_size):
+                queries.append(next(generator))    
+            total_elements += batch_size                  
+            results = model.get_last_token_weights_batch(queries, symbols)                         
+            results_od = [OrderedDict(zip(symbols, x)) for x in results]
+            results  = dict(zip(queries, results_od))          
+            cache.update(results)  
+            joblib.dump(cache, path)
