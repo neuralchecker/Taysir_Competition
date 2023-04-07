b80da56fb9416473ccfcddba34f8fef406dfa675
DESKTOP-36QKRHL\franz <franzmayrojeda@gmail.com>
2023-04-06 23:03:53 -0300

    wip

--- TAYSIR/fast_pdfa_wrapper.py
+++ TAYSIR/fast_pdfa_wrapper.py
@@ -3,9 +3,9 @@ from typing import Union
 
 class MlflowFastPDFA():
     def __init__(self, fast_pdfa: FastPDFA) -> None:
-        self.fast_pdfa = fast_pdfa
+        self._predict = fast_pdfa.sequence_probability
     
     def predict(self, model_input):
-        return self.fast_pdfa.sequence_probability(model_input)
+        return self._predict(model_input)
     
    
--- TAYSIR/faster_pdfa_wrapper.py
+++ TAYSIR/faster_pdfa_wrapper.py
@@ -3,9 +3,9 @@ from typing import Union
 
 class MlflowFasterPDFA():
     def __init__(self, faster_pdfa: FasterPDFA) -> None:
-        self.faster_pdfa = faster_pdfa
+        self._predict = faster_pdfa.sequence_probability
     
     def predict(self, model_input):
-        return self.faster_pdfa.sequence_probability(model_input)
+        return self._predict(model_input)
     
    
